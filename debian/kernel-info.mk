5f0dc56075d4da57b4f41aa5a884825fce6c6b51
Deepak Kumar <92260620+Deeepkk@users.noreply.github.com>
2024-07-18 22:29:12 +0530

    Update kernel-info.mk

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -8,23 +8,23 @@
 VARIANT = android
 
 # Kernel base version
-KERNEL_BASE_VERSION = 4.4-0
+KERNEL_BASE_VERSION = 4.9-117
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = put-cmdline-here
+KERNEL_BOOTIMAGE_CMDLINE = ine="bootopt=64S3,32N2,64N2 datapart=/dev/disk/by-partlabel/userdata androidboot.selinux=permissive buildvariant=userdebug droidian.lvm.prefer
 
 # Slug for the device vendor. This is going to be used in the KERNELRELASE
 # and package names.
-DEVICE_VENDOR = vendor
+DEVICE_VENDOR = lenovo
 
 # Slug for the device model. Like above.
-DEVICE_MODEL = device1
+DEVICE_MODEL = akita
 
 # Slug for the device platform. If unsure, keep this commented.
 # DEVICE_PLATFORM = platform
 
 # Marketing-friendly full-name. This will be used inside package descriptions
-DEVICE_FULL_NAME = Vendor Device 1
+DEVICE_FULL_NAME = Lenovo Tab M8 HD
 
 # Whether to use configuration fragments to augment the kernel configuration.
 # If unsure, keep this to 0.
@@ -40,7 +40,7 @@ KERNEL_CONFIG_USE_DIFFCONFIG = 0
 # KERNEL_PRODUCT_DIFFCONFIG = my_diffconfig
 
 # Defconfig to use
-KERNEL_DEFCONFIG = defconfig
+KERNEL_DEFCONFIG = akita_row_call_defconfig
 
 # Whether to include DTBs with the image. Use 0 (no) or 1.
 # GKI devices should set this to 0
@@ -49,7 +49,7 @@ KERNEL_IMAGE_WITH_DTB = 1
 # Path to the DTB
 # If you leave this undefined, an attempt to find it automatically
 # will be made.
-# KERNEL_IMAGE_DTB = arch/arm64/boot/dts/qcom/my_dtb.dtb
+KERNEL_IMAGE_DTB = arch/arm64/boot/dts/mediatek/mt6761.dtb
 
 # Whether to include a DTB Overlay. Use 0 (no) or 1.
 # GKI devices should set this to 0
@@ -58,7 +58,7 @@ KERNEL_IMAGE_WITH_DTB_OVERLAY = 1
 # Path to the DTB overlay.
 # If you leave this undefined, an attempt to find it automatically
 # will be made.
-# KERNEL_IMAGE_DTB_OVERLAY = arch/arm64/boot/dts/qcom/my_overlay.dtbo
+KERNEL_IMAGE_DTB_OVERLAY = arch/arm64/boot/dts/mediatek/akita_row_call.dtb
 
 # Whether to include the DTB Overlay into the kernel image
 # Use 0 (no, default) or 1.
@@ -80,12 +80,12 @@ KERNEL_IMAGE_WITH_DTB_OVERLAY_IN_KERNEL = 0
 
 # Various other settings that will be passed straight to mkbootimg
 # GKI devices can leave BASE_OFFSET, KERNEL_OFFSET, INITRAMFS_OFFSET, SECOND_OFFSET and TAGS_OFFSET empty
-KERNEL_BOOTIMAGE_PAGE_SIZE = 4096
-KERNEL_BOOTIMAGE_BASE_OFFSET = 0x00000000
+KERNEL_BOOTIMAGE_PAGE_SIZE = 2048
+KERNEL_BOOTIMAGE_BASE_OFFSET = 0x40078000
 KERNEL_BOOTIMAGE_KERNEL_OFFSET = 0x00008000
-KERNEL_BOOTIMAGE_INITRAMFS_OFFSET = 0x01000000
-KERNEL_BOOTIMAGE_SECONDIMAGE_OFFSET = 0x00f00000
-KERNEL_BOOTIMAGE_TAGS_OFFSET = 0x00000100
+KERNEL_BOOTIMAGE_INITRAMFS_OFFSET = 0x11a88000
+KERNEL_BOOTIMAGE_SECONDIMAGE_OFFSET = 0x00e88000
+KERNEL_BOOTIMAGE_TAGS_OFFSET = 0x07808000
 
 # Specify boot image security patch level if needed
 # KERNEL_BOOTIMAGE_PATCH_LEVEL = 2022-04-05
@@ -95,7 +95,7 @@ KERNEL_BOOTIMAGE_TAGS_OFFSET = 0x00000100
 
 # Required for header version 2, ignore otherwise
 # GKI devices should leave this offset empty
-KERNEL_BOOTIMAGE_DTB_OFFSET = 0x1f00000
+# KERNEL_BOOTIMAGE_DTB_OFFSET = 0x1f00000
 
 # Kernel bootimage version. Defaults to 0 (legacy header).
 # As a rule of thumb:
@@ -105,7 +105,7 @@ KERNEL_BOOTIMAGE_DTB_OFFSET = 0x1f00000
 # Devices launched with Android 11: version 2 or 3 (GKI)
 # Devices launched with Android 12: version 3 (GKI) or 4 (GKI)
 # Devices launched with Android 13: version 3 (GKI) or 4 (GKI)
-KERNEL_BOOTIMAGE_VERSION = 0
+KERNEL_BOOTIMAGE_VERSION = 1
 
 # Kernel initramfs compression. Defaults to gzip.
 # All non-gki devices need a gzip (gz) initramfs
@@ -119,7 +119,7 @@ KERNEL_INITRAMFS_COMPRESSION = gz
 # Whether to build a flashable vbmeta.img. Please note that currently
 # only empty vbmeta images (disabling verified boot) can be generated.
 # Use 0 (no) or 1 (default).
-DEVICE_VBMETA_REQUIRED = 1
+DEVICE_VBMETA_REQUIRED = 0
 
 # Samsung devices require a special flag. Enable the following if your
 # device is a Samsung device that requires flag 0 to be present
@@ -135,7 +135,7 @@ FLASH_ENABLED = 1
 
 # If your device is treble-ized, but aonly, you should set the following to
 # 1 (yes).
-FLASH_IS_AONLY = 0
+FLASH_IS_AONLY = 1
 
 # `flash-bootimage` defaults are enough for most recent devices, but legacy
 # devices won't work out of the box.
@@ -157,21 +157,21 @@ FLASH_USE_TELNET = 0
 # Device manufacturer. This must match the `ro.product.vendor.manufacturer`
 # Android property. If you don't want to specify this, leave it undefined,
 # FLASH_INFO_CPU will be checked instead.
-FLASH_INFO_MANUFACTURER = Vendor
+FLASH_INFO_MANUFACTURER = LENOVO
 
 # Device model. This must match the `ro.product.vendor.model`
 # Android property. If you don't want to specify this, leave it undefined,
 # FLASH_INFO_CPU will be checked instead.
-FLASH_INFO_MODEL = Device1
+FLASH_INFO_MODEL = Lenovo TB-8505X
 
 # Device CPU. This will be grepped against /proc/cpuinfo to check if
 # we're running on the specific device. Note this is a last-resort
 # method, specifying FLASH_INFO_MANUFACTURER and FLASH_INFO_MODEL is
 # recommended.
-FLASH_INFO_CPU = My Fictional System-on-a-Chip
+FLASH_INFO_CPU = MT8766B
 
 # Space-separated list of supported device ids as reported by fastboot
-FLASH_INFO_DEVICE_IDS = model1 model2
+FLASH_INFO_DEVICE_IDS = akita_row_call
 
 ########################################################################
 # Kernel build settings
@@ -217,4 +217,4 @@ DEB_BUILD_FOR = arm64
 KERNEL_ARCH = arm64
 
 # Kernel target to build
-KERNEL_BUILD_TARGET = Image.gz
+KERNEL_BUILD_TARGET = Image.gz-dtb
