eafca557714bffe2607f073e638b1771a6a18608
WHO53 <notwho53@gmail.com>
2024-08-25 12:08:12 +0530

    [mtk/sound] disable AudDrv spam

--- sound/soc/mediatek/mt6765/mtk-auddrv-clk.c
+++ sound/soc/mediatek/mt6765/mtk-auddrv-clk.c
@@ -340,7 +340,7 @@ void AudDrv_AUDINTBUS_Sel(int parentidx)
 	}
 	clksys_set_reg(AUDIO_CLK_CFG_4_CLR, 0x3, 0x3);
 	clksys_set_reg(AUDIO_CLK_CFG_4_SET, parentidx, 0x3);
-	pr_info("%s(), parentidx = %d, CLK_CFG_4 = 0x%08x\r\n",
+	pr_debug("%s(), parentidx = %d, CLK_CFG_4 = 0x%08x\r\n",
 		__func__, parentidx, clksys_get_reg(AUDIO_CLK_CFG_4));
 EXIT:
 	/* pr_debug("-%s()\n", __func__); */
