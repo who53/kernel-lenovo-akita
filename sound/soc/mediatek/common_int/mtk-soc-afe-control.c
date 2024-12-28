2a014410fc25595ce0af2c554de2b5dc2f246015
Deepak Kumar <notwho53@gmail.com>
2024-12-28 23:30:20 +0530

    sound/soc/mtk: stop dumping unnecessary logs
    Signed-off-by: Deepak Kumar <notwho53@gmail.com>

--- sound/soc/mediatek/common_int/mtk-soc-afe-control.c
+++ sound/soc/mediatek/common_int/mtk-soc-afe-control.c
@@ -3291,6 +3291,7 @@ void AudDrv_checkDLISRStatus(void)
 			for (index = 0; index < localctl.u4UnderflowCnt &&
 					index < DL_ABNORMAL_CONTROL_MAX;
 			     index++) {
+#if 0
 				static DEFINE_RATELIMIT_STATE(_rs, HZ, 5);
 
 				if (__ratelimit(&_rs)) {
@@ -3307,6 +3308,7 @@ void AudDrv_checkDLISRStatus(void)
 					localctl.u4HwMemoryIndex[index],
 					localctl.pucPhysBufAddr[index]);
 				}
+#endif
 			}
 		}
 	}
@@ -4357,6 +4359,7 @@ get_dlmem_frame_index(struct snd_pcm_substream *substream,
 		Afe_Block->u4DMAReadIdx += Afe_consumed_bytes;
 		Afe_Block->u4DMAReadIdx %= Afe_Block->u4BufferSize;
 		if (Afe_Block->u4DataRemained < 0) {
+#if 0
 			static DEFINE_RATELIMIT_STATE(_rs, HZ, 5);
 
 			if (__ratelimit(&_rs)) {
@@ -4364,6 +4367,7 @@ get_dlmem_frame_index(struct snd_pcm_substream *substream,
 				"[AudioWarn] u4DataRemained=0x%x, mem_block %d\n",
 				Afe_Block->u4DataRemained, mem_block);
 			}
+#endif
 		};
 		Frameidx = bytes_to_frames(substream->runtime,
 					   Afe_Block->u4DMAReadIdx);
